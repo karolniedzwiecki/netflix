1ff21bee34aec2fc740daabba97a093bf24c6ecd
Karol Niedźwiecki <niedzwiecki7@gmail.com>
2022-02-11 13:37:50 +0100

    Add rows

--- src/Requests.js
+++ src/Requests.js
@@ -4,11 +4,11 @@ const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en=US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with-genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with-genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with-genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with-genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with-genres=99`,
+    fetchActionMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=28&append_to_response=videos`,
+    fetchComedyMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=35&append_to_response=videos`,
+    fetchHorrorMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=27&append_to_response=videos`,
+    fetchRomanceMovies: `/discover/movie/?api_key=${API_KEY}&with_genres=10749&append_to_response=videos`,
+    fetchDocumentaries: `/discover/movie/?api_key=${API_KEY}&with-genres=99&append_to_response=videos`,
 };
 
 export default requests;
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from '../components/Banner.js';
 import '../components/HomeScreen.css';
 import Nav from '../components/Nav.js';
+import requests from '../Requests.js';
+import Row from './Row';
 
 function HomeScreen() {
   return (
@@ -10,10 +12,42 @@ function HomeScreen() {
         
         <Banner />
 
-        {/* ROW */}
+        <Row 
+          title='NETFLIX ORIGINALS'
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+        />
+        <Row 
+          title='Trending Now'
+          fetchUrl={requests.fetchTrending}
+        />
+        <Row 
+          title='Top Rated'
+          fetchUrl={requests.fetchTopRated}
+        />
+        <Row 
+          title='Action Movies'
+          fetchUrl={requests.fetchActionMovies}
+        />
+        <Row 
+          title='Comedy Movies'
+          fetchUrl={requests.fetchComedyMovies}
+        />
+        <Row 
+          title='Horror Movies'
+          fetchUrl={requests.fetchHorrorMovies}
+        />
+        <Row 
+          title='Romance Movies'
+          fetchUrl={requests.fetchRomanceMovies}       
+        />
+        <Row 
+          title='Documentaries'
+          fetchUrl={requests.fetchDocumentaries}        
+        />
 
     </div>
-  )
+  );
 }
 
 export default HomeScreen;
